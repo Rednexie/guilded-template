31a422a24e1e738b1faec12cde42434d7900a6cb
Rednexie <126991413+Rednexie@users.noreply.github.com>
2023-11-16 02:58:45 +0300

    Create support.js

---
+++ commands/support.js
@@ -0,0 +1,10 @@
+module.exports = {
+    config: {
+      name: "support",
+      description: "support",
+      usage: "support"
+    },
+    run: async (client, message, args) => {
+      return message.reply("https://guilded.gg/rednexie")
+    }
+  }
