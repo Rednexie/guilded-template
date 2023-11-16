180deee8f46a68b392cbe95cc8f09cac86817b4c
Rednexie <126991413+Rednexie@users.noreply.github.com>
2023-11-16 03:00:42 +0300

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -3,6 +3,7 @@ module.exports = {
       name: "support",
       description: "support",
       usage: "support"
+      aliases: ["support","contact","guilded"]
     },
     run: async (client, message, args) => {
       return message.reply("https://guilded.gg/rednexie")
